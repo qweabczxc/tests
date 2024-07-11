7f0afa5713663c5af4bcd3566a673fe321f239bb
qweabczxc <146422141+qweabczxc@users.noreply.github.com>
2024-07-11 17:13:29 +0400

    убрал повторяющиеся строки в функцию

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 const divs = document.querySelectorAll('div');
 const body = document.querySelector('body');
+const foo = (zx) => {
+    const firstColor = Math.floor(Math.random() * 256);
+    const SecondColor = Math.floor(Math.random() * 256);
+    const ThirdColor = Math.floor(Math.random() * 256);
+    const qwe = zx.style = `background-color: rgb(${firstColor}, ${SecondColor}, ${ThirdColor})`
+    localStorage.setItem(`${zx.textContent}`, qwe)
+}
 divs.forEach(div => {
     div.addEventListener('click', function(){
-        const firstColor = Math.floor(Math.random() * 256);
-        const SecondColor = Math.floor(Math.random() * 256);
-        const ThirdColor = Math.floor(Math.random() * 256);
-        const qwe = div.style = `background-color: rgb(${firstColor}, ${SecondColor}, ${ThirdColor})`
-        
-        localStorage.setItem(`${div.textContent}`, qwe)
+        foo(div)
     })
     div.style = `${localStorage.getItem(div.textContent)}`
 })
 body.addEventListener('keydown',function(event){
     const allowedKeys = ['1', '2', '3', '4', '5'];
     if(allowedKeys.includes(event.key)){
-        
-        const firstColor = Math.floor(Math.random() * 256);
-        const SecondColor = Math.floor(Math.random() * 256);
-        const ThirdColor = Math.floor(Math.random() * 256);
-        const qwe = divs[event.key - 1].style = `background-color: rgb(${firstColor}, ${SecondColor}, ${ThirdColor})`
-        localStorage.setItem(`${divs[event.key - 1].textContent}`, qwe)
+        foo(divs[event.key - 1])
     }
 })
