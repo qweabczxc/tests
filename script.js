cfea8cd2d109d3cccf0e067da519ae3d4f17bd52
qweabczxc <146422141+qweabczxc@users.noreply.github.com>
2024-07-11 14:19:06 +0400

    цвета меняются теперь ещё с помощью клавиатуры

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const divs = document.querySelectorAll('div');
+const body = document.querySelector('body');
 divs.forEach(div => {
     div.addEventListener('click', function(){
         const firstColor = Math.floor(Math.random() * 256);
@@ -9,4 +10,15 @@ divs.forEach(div => {
         localStorage.setItem(`${div.textContent}`, qwe)
     })
     div.style = `${localStorage.getItem(div.textContent)}`
+})
+body.addEventListener('keydown',function(event){
+    const allowedKeys = ['1', '2', '3', '4', '5'];
+    if(allowedKeys.includes(event.key)){
+        
+        const firstColor = Math.floor(Math.random() * 256);
+        const SecondColor = Math.floor(Math.random() * 256);
+        const ThirdColor = Math.floor(Math.random() * 256);
+        const qwe = divs[event.key - 1].style = `background-color: rgb(${firstColor}, ${SecondColor}, ${ThirdColor})`
+        localStorage.setItem(`${divs[event.key - 1].textContent}`, qwe)
+    }
 })
